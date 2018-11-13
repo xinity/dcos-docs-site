654c0715974c9747081dd1e7e550d46bbb109c23
niartenyaw <amwayne90@gmail.com>
2018-11-13 14:15:41 -0800

    Fix bad code blocks and add to script

--- scripts/localization-update-links.sh
+++ scripts/localization-update-links.sh
@@ -7,5 +7,14 @@ find ./pages/zh -type f -exec sed -i '' -e 's/](1.11/](zh\/1.11/g' {} \;
 
 find ./pages/zh -type f -exec sed -i '' -e 's/#include \/cn/#include \/zh/g' {} \;
 
+# bad code blocks
+
+# Opening
+find ./pages/zh -type f -exec sed -i '' -e 's/<```>/```/g' {} \;
+# Closing
+find ./pages/zh -type f -exec sed -i '' -e 's/<\/```>/```/g' {} \;
+# linebreaks
+find ./pages/zh -type f -exec sed -i '' -e 's/<linebreak>/\'$'\n/g' {} \;
+
 # Data yml
 
